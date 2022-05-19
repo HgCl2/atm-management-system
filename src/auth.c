bae5eee28bd0d14008538f7798e19d2c216d53a5
Serik Almas <s.almas_99@mail.ru>
2022-05-19 13:52:14 +0600

    fixed bug with login for first time

--- src/auth.c
+++ src/auth.c
@@ -37,15 +37,17 @@ const char *getPassword(struct User u)
 {
     FILE *fp;
     struct User userChecker;
-
+    char id[5];
     if ((fp = fopen("./data/users.txt", "r")) == NULL)
     {
         printf("Error! opening file");
         exit(1);
     }
 
-    while (fscanf(fp, "%s %s", userChecker.name, userChecker.password) != EOF)
+    while (fscanf(fp, "%s %s %s", id, userChecker.name, userChecker.password) != EOF)
     {
+        printf("%s %s\n", userChecker.name, userChecker.password);
+        printf("%s %s\n", u.name, u.password);
         if (strcmp(userChecker.name, u.name) == 0)
         {
             fclose(fp);
@@ -56,4 +58,33 @@ const char *getPassword(struct User u)
 
     fclose(fp);
     return "no user found";
+}
+
+void registerMenu(char a[50], char pass[50]){
+    struct termios oflags, nflags;
+
+    system("clear");
+    printf("\n\n\n\t\t\t\t   Bank Management System\n\t\t\t\t\t User Login:");
+    scanf("%s", a);
+
+    // disabling echo
+    tcgetattr(fileno(stdin), &oflags);
+    nflags = oflags;
+    nflags.c_lflag &= ~ECHO;
+    nflags.c_lflag |= ECHONL;
+
+    if (tcsetattr(fileno(stdin), TCSANOW, &nflags) != 0)
+    {
+        perror("tcsetattr");
+        return exit(1);
+    }
+    printf("\n\n\n\n\n\t\t\t\tEnter the password to login:");
+    scanf("%s", pass);
+
+    // restore terminal
+    if (tcsetattr(fileno(stdin), TCSANOW, &oflags) != 0)
+    {
+        perror("tcsetattr");
+        return exit(1);
+    }
 }
