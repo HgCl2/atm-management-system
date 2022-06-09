92de686a931e61630424ef43cb7ab40087df8e6c
Serik Almas <s.almas_99@mail.ru>
2022-06-09 23:13:11 +0600

    correction of isContain

--- src/additional.c
+++ src/additional.c
@@ -2,12 +2,12 @@
 
 bool isContain(char* array[100], char* str){
     for (int i = 0; i < 100; i++){
-        if (array[i] == str){
-            return false;
+        if (strcmp(array[i], str) == 0){
+            return true;
         }
     }
 
-    return true;
+    return false;
 }
 
 bool IsNameContained(char *name){
